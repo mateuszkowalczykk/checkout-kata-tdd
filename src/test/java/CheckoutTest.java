095ad48bd50f406ae3ae3cab9ed97ac276128d4b
mateuszkowalczykk <x625134x@gmail.com>
2020-11-12 12:45:18 +0100

    {RED} can scan item

--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -1,9 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CheckoutTest {
     Checkout checkout;
+    @BeforeEach
+    void setUp() {
+        checkout = new Checkout("src/main/resources/pricing_rules.txt");
+    }
+
     @Test
     void canCreateCheckOut() {
         checkout = new Checkout("src/main/resources/pricing_rules.txt");
+    }
+
+    @Test
+    void canScanItem() {
+        checkout.scan("A");
     }
 }
