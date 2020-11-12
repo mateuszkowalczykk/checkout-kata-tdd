fa94e0bfde1f03dee1d8296cb51257d498a38f98
mateuszkowalczykk <x625134x@gmail.com>
2020-11-12 13:46:37 +0100

    {GREEN} can scan items and get total price

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -1,14 +1,35 @@
+import java.io.File;
+import java.util.*;
+
 public class Checkout {
+    List<String> scannedItems = new ArrayList<>();
+    Map<String,Integer> pricingRules = new HashMap<>();
 
     public Checkout(String path) {
+        try {
+            File file = new File(path);
+            Scanner loadedFile = new Scanner(file);
+            while (loadedFile.hasNextLine()) {
+                String[] itemData = loadedFile.nextLine().split(";");
+                String itemName = itemData[0];
+                int price = Integer.parseInt(itemData[1]);
+                pricingRules.put(itemName,price);
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public void scan(String ItemName){
-
+    public void scan(String itemName){
+        scannedItems.add(itemName);
     }
 
     public int getTotalPrice(){
-        return 0;
+        int totalPrice = 0;
+        for(String itemName : scannedItems){
+            totalPrice += pricingRules.get(itemName);
+        }
+        return totalPrice;
     }
 
 
